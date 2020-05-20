70bdfc3c412223555e24da1c60d3c55548b45d00
Ahmad Wael <dev.ahmedwael@gmail.com>
2020-05-21 01:07:30 +0200

    initial release

---
+++ wp-optimizes.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Optimizes
+ * Description: Disable wp cron - reduce ajax requests - disable wp heartbeat.
+ * Version: 1.0
+ * Author: Ahmad Wael
+ * Author URI: https://github.com/devwael
+ * License: GPLv2
+ */
